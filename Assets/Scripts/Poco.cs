123f59fe0923de2f4d9389e2cd4e70c3b5e02383
2manydisplays <der-hoffmann@gmx.de>
2021-06-17 14:36:43 +0200

    Added NavMesh Navigation

--- Assets/Scripts/Poco.cs
+++ Assets/Scripts/Poco.cs
@@ -1,23 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Poco : MonoBehaviour
-{
-    [SerializeField] float moveSpeed = 10f;
+{    
+    [SerializeField] float rotationSpeed = 1;
+
+    public NavMeshAgent agent;   
+
+    bool isMoving = false;
+    bool isWaiting = false;
+
+    public LayerMask ground;
+
+    Vector3 targetPosition;   
+    Quaternion newPlayerRot;    
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {      
+        //rotation is controlled by animator
+        //agent.updateRotation = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue= Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float zValue= Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        if (Input.GetMouseButton(0))
+        {
+            SetAgentTarget();
+        }
 
-        transform.Translate(xValue,0,zValue);
+        if (isMoving == true)
+        {
+            updateMovement();
+        }
     }
+
+    void SetAgentTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100, ground))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+
+    void updateMovement()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, newPlayerRot, rotationSpeed * Time.deltaTime); 
+
+        if (transform.position == targetPosition)
+        {
+            isMoving = false;            
+        }
+    }
+
 }
