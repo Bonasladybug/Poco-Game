7079ae4f1fbae9fc218f390857b68a57115746b2
Bonasladybug <bonasladybug@gmail.com>
2021-06-19 12:58:38 +0200

    Npc player

---
+++ Assets/Scripts/NpcPlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcPlayer : MonoBehaviour
+{
+    public GameObject chat;
+    public AudioSource CollectSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter () {
+		chat.SetActive(true);
+        CollectSound.Play ();
+
+		
+    }
+}
